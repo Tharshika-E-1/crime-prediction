d05d071cb79f53c89037a369641c318f32cdf29a
Tharshika-E-1 <tharshikae1@gmail.com>
2026-06-06 12:02:40 +0530

    Configure Flask app to use environment port

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pandas as pd
 from sklearn.cluster import DBSCAN
+import os
 
 
 # ---------------------------------
@@ -179,4 +180,5 @@ def admin_recent():
 # ---------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
